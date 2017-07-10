33155da39764578f753f681d1773ffaccc762029
elroy <elroy@symbaloo.com>
2017-07-10 22:58:22 +0200

    Update start-application.bat

--- start-application.bat
+++ start-application.bat
@@ -1,10 +1,11 @@
+echo off
 set sp=--server.port=443
 set sks=--server.ssl.key-store=classpath:poloniex.keystore
 set sksp=--server.ssl.key-store-password=poloniex
 set skp=--server.ssl.key-password=poloniex
-set gbl=--gunbot.location=file://C:/Users/Elroy/Documents/GUNBOT_v3.3.2
+set gbl=--gunbot.location=file://C:/Users/Taniman/Documents/GUNBOT_v3.3.2
 
-set lnl=--logging.level.nl.komtek=DEBUG
+REM set lnl=--logging.level.nl.komtek=DEBUG
 
 REM This apiKey pair will do all the heavy lifting
 set da=--default_apiKey=first api key
@@ -14,5 +15,5 @@ REM This apiKey will do the buying and selling and orders canceling stuff. You c
 set a=--apiKey1=another apiKey
 set as=--apiSecret1=your secret
 
-java -jar GunbotProxyCommunity-0.9.5.jar %sp% %sks% %sksp% %skp% %da% %ds% %a% %as% %lnl%
+java -jar GunbotProxyCommunity-0.9.7.jar %sp% %sks% %sksp% %skp% %da% %ds% %a% %as% %lnl%
 pause
